9a5edf2a8612e5945cdbc8903d25f04cc0716449
Hernan Zaracho <hernanandreszaracho@gmail.com>
2025-11-06 17:45:00 -0300

    Configurar URL del backend para producción

--- frontend/src/config/environment.ts
+++ frontend/src/config/environment.ts
@@ -3,7 +3,9 @@ const isDevelopment =
   window.location.hostname === "127.0.0.1";
 
 export const config = {
-  API_BASE_URL: isDevelopment ? "http://localhost:5000" : "",
+  API_BASE_URL: isDevelopment
+    ? "http://localhost:5000"
+    : "https://ecommerce-tienda-ropa-backend.onrender.com",
 
   isDevelopment,
   isProduction: !isDevelopment,
